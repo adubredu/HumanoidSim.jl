e97aa5a0c7fc58f3049b4e57cae7d0ba71ee0f02
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2022-10-17 01:26:56 -0400

    updated env

--- src/types.jl
+++ src/types.jl
@@ -3,16 +3,19 @@ mutable struct Env
     trajectories::Dict
     dynamics::Dict
     init_position::Dict
+    geometries::Dict
 
     function Env(objects)
         trajectories = Dict()
         dynamics = Dict()
         init_position = Dict()
+        geometries = Dict()
         for obj in objects
             trajectories[obj]=[]
             dynamics[obj]=nothing
             init_position[obj]=nothing
+            geometries[obj]=[]
         end
-        new(objects, trajectories, dynamics, init_position)
+        new(objects, trajectories, dynamics, init_position, geometries)
     end
 end
